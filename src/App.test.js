3130b6b3da10018c8b5c27c97630b849427a4ed9
EUNICE <eunicennaji@hotmail.co.uk>
2024-05-24 19:11:57 +0200

    Updated code to allow testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-/*import React from 'react';
+import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('checks for text in homepage', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText( "List. Buy. Check");
   expect(linkElement).toBeInTheDocument();
-});*/
+});
--- src/components/Home.js
+++ src/components/Home.js
@@ -18,15 +18,16 @@ let Home = () => {
     'Web version also available',
     'Shopping is fun with IShopSmart',
   ];
-  const [feature, setFeature] = useState(features.feature1);
+  const [feature, setFeature] = useState(features[0]);
 
   useEffect(() => {
-    var count = 6;
-    setInterval(() => {
-      count === 6 ? (count = 0) : count++;
+    let count = 0;
+    const interval = setInterval(() => {
+      count = (count + 1) % features.length;
       setFeature(features[count]);
-    }, 3000);
-  });
+    }, 3000); // Adjusted to 3000ms for better readability
+    return () => clearInterval(interval);
+  }, [features]);
 
   return (
     <BrowserRouter>
@@ -34,52 +35,55 @@ let Home = () => {
         <Route exact path='/Login' component={LoginPage} />
         <Route path='/MyLists' exact component={MyLists} />
         <Route path='/PrivacyPolicy' exact component={PrivacyPolicy} />
-        <div className='homeFullBackground'>
-          <div className='homeNavigation'>
-            <span className='navHomeText'>IShopSmart</span>
-            <Link exact to='/Login' className='navHomeText2'>
-              <span>Login/SignUp</span>
-            </Link>
-          </div>
-          <div className='homeBackground'>
-            <div className='ishopsmartText'>ISHOPSMART</div>
-            <div className='secondaryTextHome'>List. Buy. Check</div>
-          </div>
-          <div className='HomeSection2'>
-            <img src={phoneScreenShot} alt='phone' className='phoneImage' />
-            <span className='textBoard'>{feature}</span>
-          </div>
-          <div className='HomeSection3'>
-            <img
-              src={iShopSmartLogo}
-              className='iShopSmartLogo'
-              alt='logo for ishopsmart'
-            />
-            <a href='https://play.google.com/store/apps/details?id=com.euniclab.ishopsmart'>
+        <Route path='/'>
+          <div className='homeFullBackground'>
+            <div className='homeNavigation'>
+              <span className='navHomeText'>IShopSmart</span>
+              <Link to='/Login' className='navHomeText2'>
+                <span>Login/SignUp</span>
+              </Link>
+            </div>
+            <div className='homeBackground'>
+              <div className='ishopsmartText'>ISHOPSMART</div>
+              <div className='secondaryTextHome'>List. Buy. Check</div>
+            </div>
+            <div className='HomeSection2'>
+              <img src={phoneScreenShot} alt='phone' className='phoneImage' />
+              <span className='textBoard'>{feature}</span>
+            </div>
+            <div className='HomeSection3'>
               <img
-                src={googleBadge}
-                className='googleBadge'
-                alt='google badge'
+                src={iShopSmartLogo}
+                className='iShopSmartLogo'
+                alt='logo for ishopsmart'
               />
-            </a>
-            <Link exact to='/MyLists' className='webBadge'>
-              <div className='webBadge'>Use Web Version</div>
-            </Link>
+              <a href='https://play.google.com/store/apps/details?id=com.euniclab.ishopsmart'>
+                <img
+                  src={googleBadge}
+                  className='googleBadge'
+                  alt='google badge'
+                />
+              </a>
+              <Link to='/MyLists' className='webBadge'>
+                <div className='webBadge'>Use Web Version</div>
+              </Link>
+            </div>
+            <div className='footer'>
+              <a className='footerContactUs' href='mailto:eunicLab.co@gmail.com'>
+                Contact us
+              </a>
+              <span className='footerDesigner'>
+                Powered By <a href='https://github.com/eunicLab'>eunicLab</a>
+              </span>
+              <Link to='/PrivacyPolicy' className='footerPrivacyPolicy'>
+                Privacy Policy
+              </Link>
+            </div>
           </div>
-          <div className='footer'>
-            <a className='footerContactUs' href='mailto:eunicLab.co@gmail.com'>
-              Contact us
-            </a>
-            <span className='footerDesigner'>
-              Powered By <a href='https://github.com/eunicLab'>eunicLab</a>
-            </span>
-            <Link exact to='/PrivacyPolicy' className='footerPrivacyPolicy'>
-              Privacy Policy
-            </Link>{' '}
-          </div>
-        </div>
+        </Route>
       </Switch>
     </BrowserRouter>
   );
 };
+
 export default Home;
